fb8771c76a541e916d15c9e7637d29c475f6b4b3
Hayato0227 <fai227@outlook.jp>
2022-12-12 13:52:38 +0900

    Commit

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
+const fs = require("fs");
 const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 8080;
-const db = require("./scripts/database");
+
+const rankingNum = 10;
+let ranking = [];
+ranking = JSON.parse(fs.readFileSync("database.json"));
 
 app.use(express.static("public"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/ranking", (req, res) => {
+    res.json(ranking);
+})
 
+app.post("/ranking", (req, res) => {
+    ranking.push(req.body);
+    ranking.sort((a, b) => {
+
+    });
+    res.json(ranking);
 });
-
 
 
 app.listen(port, () => {
